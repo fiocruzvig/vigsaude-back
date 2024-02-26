2c1279386bfdc5a16346f1776c25445f2e496710
SEAC/ENSP <apoio@ensp.fiocruz.br>
2024-02-26 11:39:03 -0300

    Add Cors

--- example/views.py
+++ example/views.py
@@ -1,8 +1,13 @@
 # example/views.py
 from django.http import HttpResponse
 import json
+import requests
 
 def index(request):
     data = {'key': 'valor'}
+    #r = requests.get('https://jsonplaceholder.typicode.com/users')
+    #print(r)
+    #usuarios = r.json()
+    #print(requests)
     response_data = json.dumps(data)
     return HttpResponse(response_data, content_type="application/json")
