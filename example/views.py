9132ebc2c8cec3b900eb011d03133e34d972a4d0
SEAC/ENSP <apoio@ensp.fiocruz.br>
2024-02-26 12:35:10 -0300

    Add Again

--- example/views.py
+++ example/views.py
@@ -2,18 +2,18 @@
 from django.http import HttpResponse
 import json
 #import requests
-#from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 
-#@csrf_exempt
+@csrf_exempt
 def index(request):
-    '''
+    
     if request.method == 'POST':
         data = {'key': 'POST'}
         print("Aqui")
         response_data = json.dumps(data)
         return HttpResponse(response_data, content_type="application/json")
-    '''
+    
     return HttpResponse("Olá")
 
     # data = {'key': 'GET'}
