788093d5952bbad3a50ece6aac9ea2b50d2300d1
SEAC/ENSP <apoio@ensp.fiocruz.br>
2024-02-26 10:23:05 -0300

    Teste

--- example/views.py
+++ example/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 import json
 
 def index(request):
-    data = {'key': 'value'}
+    data = {'key': 'valor'}
     response_data = json.dumps(data)
     return HttpResponse(response_data, content_type="application/json")
