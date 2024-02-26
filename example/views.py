78ea1ad26bc04142ae133e7cba30a02160f13177
SEAC/ENSP <apoio@ensp.fiocruz.br>
2024-02-26 12:29:47 -0300

    remove posth method

--- example/views.py
+++ example/views.py
@@ -7,12 +7,13 @@ import requests
 
 #@csrf_exempt
 def index(request):
-    print(request)
+    '''
     if request.method == 'POST':
         data = {'key': 'POST'}
         print("Aqui")
         response_data = json.dumps(data)
         return HttpResponse(response_data, content_type="application/json")
+    '''
     return HttpResponse("Olá")
 
     # data = {'key': 'GET'}
