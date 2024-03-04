ea47cf7fbcf4b4c8c708974b02ef479e1e03fb9e
SEAC/ENSP <apoio@ensp.fiocruz.br>
2024-03-04 10:01:29 -0300

    Add Json Response

--- example/views.py
+++ example/views.py
@@ -1,7 +1,6 @@
 # example/views.py
 from django.http import HttpResponse
 import json
-#import requests
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -10,8 +9,11 @@ from django.views.decorators.csrf import csrf_exempt
 def login(request):
     
     if request.method == 'POST':
-        response = JsonResponse({'key': 'POST'})    
-        #print(request["usuario"])
+
+        body_unicode = request.body.decode('utf-8')  
+        body = json.loads(body_unicode)       
+        response = JsonResponse(body)
+
         return HttpResponse(response, content_type="application/json")
     
   
