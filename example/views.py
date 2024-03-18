64eee028151ba6a0dee9451fc1f70993f5702fb6
SEAC/ENSP <apoio@ensp.fiocruz.br>
2024-03-18 14:04:07 -0300

    Change require post

--- example/views.py
+++ example/views.py
@@ -4,7 +4,7 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-from example.exceptions.Exceptions import require_post
+from example.exceptions.Exceptions import require_login
 import json
 
 
