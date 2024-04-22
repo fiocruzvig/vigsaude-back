4dce85a9680dda116b9bb568e131f39e7a4a8b69
afcarioca <afcarioca@hotmail.com>
2024-04-21 21:09:21 -0300

    Add Token Response

--- example/views.py
+++ example/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from example.exceptions.Exceptions import require_post
+from rest_framework_simplejwt.tokens import RefreshToken
 from  example.LoginFormRequest.loginFormRequest import LoginFormRequest
 from example.services.login_service import LoginService
 
@@ -24,11 +25,12 @@ def login(request):
            errors = dict(form.errors.items())
            return HttpResponse(JsonResponse({'status': 'Erro', 'message': 'Erros de Validação', 'errors': errors}),  content_type="application/json", status= 400) 
 
-        
-        if LoginService().auth(data["username"], data["password"]) is None:
+        user = LoginService().auth(data["username"], data["password"])
+        if user is None:
                 return HttpResponse(JsonResponse({'status': 'Acesso Negado', 'message':'Usuário Inexistente'}), content_type="application/json", status=403)
         
-        return HttpResponse(JsonResponse({'status': 'OK', 'message':'Sucesso'}), content_type="application/json", status=200)
+        refresh = RefreshToken.for_user(user)
+        return HttpResponse(JsonResponse({'status': 'OK', 'message':'Sucesso', 'Refresh':str(refresh),'Token':str(refresh.access_token)}), content_type="application/json", status=200)
 
     
   
