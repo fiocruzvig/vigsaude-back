296ae863428bb3569b583ddeee8afcd2f8d22fc6
afcarioca <afcarioca@hotmail.com>
2024-04-23 13:21:27 -0300

    Add new toke features

--- example/controllers/login_view.py
+++ example/controllers/login_view.py
@@ -7,8 +7,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from  example.dtos.request.loginFormRequest import LoginFormRequest
 from example.services.login_service import LoginService
-from django.core.serializers import serialize
-from django.contrib.auth import login
+
 
 import json
 
