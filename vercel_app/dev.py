185a823ab8a82dd0c6bed95fcfe6a008df4d29b2
afcarioca <afcarioca@hotmail.com>
2024-04-23 14:59:43 -0300

    Add module psycopg

--- vercel_app/dev.py
+++ vercel_app/dev.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 DATABASES = {
 
     'default':{
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg',
         'NAME': os.getenv('NAME'),
         'USER':os.getenv('USER'),
         'PASSWORD':os.getenv('PASSWORD'),
