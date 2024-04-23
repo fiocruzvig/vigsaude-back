ab969cbd31080b8bbbc9c9bb4c0314e04502c1c0
afcarioca <afcarioca@hotmail.com>
2024-04-23 14:39:55 -0300

    Add tables in Cloud

--- vercel_app/prod.py
+++ vercel_app/prod.py
@@ -1,15 +1,25 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 from .base import *
+load_dotenv() 
 
 
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv("API_KEY")
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 DATABASES = {
-    
+
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME_PG'),
+        'USER':os.getenv('USER_PG'),
+        'PASSWORD':os.getenv('PASSWORD_PG'),
+        'HOST':os.getenv('HOST_PG'),
+        'PORT':os.getenv('PORT_PG')
+    }
+
 }
 
 
